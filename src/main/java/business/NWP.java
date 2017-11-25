bb86fc2a9f757fbe94f81c6c7ff190e92679a954
binio <tbiniecki@gmail.com>
2017-11-25 00:39:00 +0000

    method Euclides way

--- src/main/java/business/NWP.java
+++ src/main/java/business/NWP.java
@@ -1,20 +1,33 @@
 package business;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class NWP {
-    int nwp;
-    int a;
-    int b;
 
-    public NWP(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-
-
-    public static int calculate(int p, int q) {
+    public int calculate(int p, int q) {
         if (q == 0) {
             return p;
         }
         return calculate(q, p % q);
+    }
+
+    public int calculateSteps(int p, int q) {
+        if(q==0){
+            return p;
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(p);
+        list.add(q);
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        if(list.get(0)-list.get(1) > 0) {
+            System.out.println(list.get(0)-list.get(1)+",");
+            return calculateSteps(list.get(1),list.get(0)-list.get(1));
+        }
+            return list.get(1);
+
     }
 }
