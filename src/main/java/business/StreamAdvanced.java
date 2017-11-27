2d27643a27d559fc485d855370eb2d8ecfb3ccef
binio <tbiniecki@gmail.com>
2017-11-27 22:52:59 +0000

    add groupBy

--- src/main/java/business/StreamAdvanced.java
+++ src/main/java/business/StreamAdvanced.java
@@ -2,6 +2,7 @@ package business;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
@@ -13,7 +14,7 @@ public class StreamAdvanced {
     public StreamAdvanced(){
         people.add(new Person("Thomas",44));
         people.add(new Person("Adam",40));
-        people.add(new Person("Martin",34));
+        people.add(new Person("Martin",24));
         people.add(new Person("Simon",24));
         people.add(new Person("Brian",14));
         people.add(new Person("Tom",14));
@@ -26,5 +27,12 @@ public class StreamAdvanced {
                 .collect(Collectors.toList());
 
         list.forEach(person -> System.out.println(person.name + " " + person.getAge()));
+    }
+
+    public void groupByAge(){
+        Map<Integer, List<Person>> personsByAge = people.stream()
+                .collect(Collectors.groupingBy(p->p.getAge()));
+        personsByAge
+                .forEach((age, p) -> System.out.format("age %s: %s\n", age, p));
     }
 }
