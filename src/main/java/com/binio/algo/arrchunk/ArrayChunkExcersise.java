a3d06b7d99e8874ba57d415a055302d38e5d1339
binio <tbiniecki@gmail.com>
2020-04-03 19:25:11 +0100

    practice chunking ;-)

--- src/main/java/com/binio/algo/arrchunk/ArrayChunkExcersise.java
+++ src/main/java/com/binio/algo/arrchunk/ArrayChunkExcersise.java
@@ -6,6 +6,7 @@ public class ArrayChunkExcersise {
 
     //complete the method
     public static int[][] chunk(int[] arrayToSplit, int chunkSize) {
+        String info1 = "take 1";
 //        int rest = arrayToSplit.length % chunkSize;
 //        int sizeOfNewArray = arrayToSplit.length/chunkSize + (rest == 0 ? 0:1);
 //        int[][] newArray = new int[sizeOfNewArray][];
@@ -19,6 +20,35 @@ public class ArrayChunkExcersise {
 //        }
 //
 //        return newArray;
-        return null;
+
+        String info2 = "take 2";
+//        int rest = arrayToSplit.length % chunkSize;
+//        int newArraySize = arrayToSplit.length/chunkSize + (rest>0? 1 : 0);
+//        int[][] newArray = new int[newArraySize][];
+//
+//        for(int i=0; i<(rest >0 ? newArraySize-1 : newArraySize); i++) {
+//            newArray[i] = Arrays.copyOfRange(arrayToSplit, i * chunkSize, i * chunkSize + chunkSize);
+//        }
+//
+//        if(rest>0){
+//            newArray[newArraySize-1] = Arrays.copyOfRange(arrayToSplit, (newArraySize - 1) * chunkSize, (newArraySize - 1) * chunkSize + rest);
+//        }
+//        return newArray;
+
+        String info3 = "take 3";
+
+        int rest = arrayToSplit.length % chunkSize;
+        int newArrayLength = arrayToSplit.length / chunkSize + (rest > 0 ? 1:0);
+        int[][] newArray = new int[newArrayLength][];
+
+        for(int i=0; i<(rest > 0 ? newArrayLength -1 : newArrayLength); i++) {
+            newArray[i] = Arrays.copyOfRange(arrayToSplit, i*chunkSize, i*chunkSize + chunkSize);
+        }
+
+        if(rest > 0) {
+            newArray[newArrayLength-1] = Arrays.copyOfRange(arrayToSplit, (newArrayLength-1)*chunkSize, (newArrayLength-1)*chunkSize + rest);
+
+        }
+        return newArray;
     }
 }
--- src/test/java/com/binio/algo/arrchunk/ArrayChunkExcersiseTest.java
+++ src/test/java/com/binio/algo/arrchunk/ArrayChunkExcersiseTest.java
@@ -8,6 +8,13 @@ public class ArrayChunkExcersiseTest {
 
     @Test
     public void chunk() {
+        int[] input = {2,3,4,5,6,76,1,1};
+        int[][] expected = {{2,3},{4,5},{6,76},{1,1}};
+        assertArrayEquals(expected, ArrayChunkExcersise.chunk(input, 2));
+    }
+
+    @Test
+    public void chunk2() {
         int[] input = {2,3,4,5,6,76,1};
         int[][] expected = {{2,3},{4,5},{6,76},{1}};
         assertArrayEquals(expected, ArrayChunkExcersise.chunk(input, 2));
