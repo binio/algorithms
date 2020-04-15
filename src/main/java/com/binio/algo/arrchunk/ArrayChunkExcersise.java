e1ccf9c4d009f45eb24dffde93858e6b3f57681e
binio <tbiniecki@gmail.com>
2020-04-15 08:23:16 +0100

    take 4

--- src/main/java/com/binio/algo/arrchunk/ArrayChunkExcersise.java
+++ src/main/java/com/binio/algo/arrchunk/ArrayChunkExcersise.java
@@ -37,18 +37,35 @@ public class ArrayChunkExcersise {
 
         String info3 = "take 3";
 
+//        int rest = arrayToSplit.length % chunkSize;
+//        int newArrayLength = arrayToSplit.length / chunkSize + (rest > 0 ? 1:0);
+//        int[][] newArray = new int[newArrayLength][];
+//
+//        for(int i=0; i<(rest > 0 ? newArrayLength -1 : newArrayLength); i++) {
+//            newArray[i] = Arrays.copyOfRange(arrayToSplit, i*chunkSize, i*chunkSize + chunkSize);
+//        }
+//
+//        if(rest > 0) {
+//            newArray[newArrayLength-1] = Arrays.copyOfRange(arrayToSplit, (newArrayLength-1)*chunkSize, (newArrayLength-1)*chunkSize + rest);
+//
+//        }
+//        return newArray;
+
+        String info4 = "take 4";
+
         int rest = arrayToSplit.length % chunkSize;
-        int newArrayLength = arrayToSplit.length / chunkSize + (rest > 0 ? 1:0);
-        int[][] newArray = new int[newArrayLength][];
+        int newArraySize = arrayToSplit.length / chunkSize + (rest > 0? 1:0);
+        int[][] newArray = new int[newArraySize][];
 
-        for(int i=0; i<(rest > 0 ? newArrayLength -1 : newArrayLength); i++) {
-            newArray[i] = Arrays.copyOfRange(arrayToSplit, i*chunkSize, i*chunkSize + chunkSize);
+        for(int i=0; i<(rest>0? newArraySize - 1: newArraySize); i++) {
+            newArray[i] = Arrays.copyOfRange(arrayToSplit,i*chunkSize, i*chunkSize+chunkSize);
         }
 
-        if(rest > 0) {
-            newArray[newArrayLength-1] = Arrays.copyOfRange(arrayToSplit, (newArrayLength-1)*chunkSize, (newArrayLength-1)*chunkSize + rest);
-
+        if(rest>0) {
+            newArray[newArraySize-1] = Arrays.copyOfRange(arrayToSplit, (newArraySize-1)*chunkSize, (newArraySize-1)*chunkSize +rest);
         }
+
         return newArray;
+
     }
 }
